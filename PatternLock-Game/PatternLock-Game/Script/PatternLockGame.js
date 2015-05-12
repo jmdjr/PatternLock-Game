4e9e9f73f05ac825bff091c7837c8670cf249220
jmdjr <jmdcodejunkie@gmail.com>
2015-05-12 16:32:32 -0700

    update with phaser
    Getting a phaser.js going which is starting to look good, that will
    replace the old game.

---
+++ PatternLock-Game/PatternLock-Game/Script/PatternLockGame.js
@@ -0,0 +1,14 @@
+﻿var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+
+function preload() {
+    game.load.spritesheet('buttons', 'Resources/ButtonsSpriteSheet.png', 34, 34, 7);
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
